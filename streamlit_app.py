6507ad1deeba70c61ce12a9238ce4877e062b713
Marcela Caetano <128152933+MarcelaCAE@users.noreply.github.com>
2024-12-19 17:19:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+
 st.title('🎈 HousePrediction-machine-learning')
 
 st.info('This is a machine learning model')
 
+with st.expander('Dataset'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/MarcelaCAE/HousePrediction-machine-learning/refs/heads/master/king_%20country_%20houses_aa.csv')
+  df
