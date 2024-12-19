a5008b3032c16b5d7e2269e7c27ac7a62adfa920
Marcela Caetano <128152933+MarcelaCAE@users.noreply.github.com>
2024-12-19 19:10:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -242,11 +242,11 @@ with st.expander("Data Modeling", expanded=True):
 # Model 2 -Normalization of the Data
 # Para implementar melhorias adicionais como normalização e remoção de outliers:
 
-   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-   scaler = StandardScaler()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
 
-   X_train_scaled = scaler.fit_transform(X_train) # assuming now the normalization
-   X_test_scaled = scaler.transform(X_test)
+    X_train_scaled = scaler.fit_transform(X_train) # assuming now the normalization
+    X_test_scaled = scaler.transform(X_test)
 
  # Initiating the Xboostedd Model
    XGboosted_model_normalize = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1)
